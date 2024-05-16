a4e35a3fbf8515bc874370d83501b3c9388c9411
Jeremy Louie <jeremysf74@yahoo.com>
2024-05-16 10:09:03 -0700

    Installed Tailwind

---
+++ src/Nonprofit.tsx
@@ -0,0 +1,14 @@
+
+function Nonprofit() {
+    return (
+        <>
+            <p>NonProfit</p>
+            <div>
+                <input name="fullName" />
+                <input name="loanAmount" />
+            </div>
+        </>
+    )
+}
+
+export default Nonprofit
